539ccd8f801b6783a8aee125cff719133d6dd9b1
NikKnez <nknezevic07@gmail.com>
2024-02-29 13:33:03 +0100

    Added exceptions

---
+++ src/main/java/ExamPortal/exception/CourseSaveFailedException.java
@@ -0,0 +1,16 @@
+package ExamPortal.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class CourseSaveFailedException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public CourseSaveFailedException(String message) {
+		super(message);
+	}
+
+}
+
